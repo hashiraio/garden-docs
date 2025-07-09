f42c551d2fbbf5eb18ab4e27d992aef16b3ade90
SATHVIK SVS <svssathvik77@gmail.com>
2025-07-09 14:50:03 +0530

    add testnet and mainnet supported assets

--- snippets/SupportedAssets.jsx
+++ snippets/SupportedAssets.jsx
@@ -1,4 +1,4 @@
-export const supportedAssetsConfig = [
+export const mainnetSupportedAssetsConfig = [
   {
     chain: { name: "Arbitrum", logo: "/icons/arbitrum.svg" },
     assets: [
@@ -74,6 +74,63 @@ export const supportedAssetsConfig = [
   }
 ];
 
+export const testnetSupportedAssetsConfig = [
+  {
+    chain: { name: "Arbitrum Sepolia", logo: "/icons/arbitrum.svg" },
+    assets: [
+      { symbol: "WBTC", logo: "/icons/wbtc.svg", contract: "https://sepolia.arbiscan.io/address/0x1cd0bBd55fD66B4C5F7dfE434eFD009C09e628d1" },
+      { symbol: "SEED", logo: "/icons/seed.svg", contract: "https://sepolia.arbiscan.io/address/0xD5FeDb4ceCB0F1D32788a190d9EB47D94D23eE4e" },
+      { symbol: "iBTC", logo: "/icons/iBTC.png", contract: "https://sepolia.arbiscan.io/address/0xDfe6D9363EE96152D39391009a6723819D9E25EB" }
+    ]
+  },
+  {
+    chain: { name: "Base Sepolia", logo: "/icons/base.svg" },
+    assets: [
+      { symbol: "WBTC", logo: "/icons/wbtc.svg", contract: "https://sepolia.basescan.org/address/0x00ab86f54F436CfE15253845F139955ae0C00bAf" },
+      { symbol: "USDT", logo: "/icons/usdt.svg", contract: "https://sepolia.basescan.org/address/0xB391CA6D0A76CD2A927bC314856E8a374a225CFc" },
+      { symbol: "iBTC", logo: "/icons/iBTC.png", contract: "https://sepolia.basescan.org/address/0xBCdAD29aC77E5bb27FD528Ab0045AF630259FE4f" }
+    ]
+  },
+  {
+    chain: { name: "Bitcoin Testnet", logo: "/icons/btc.svg" },
+    assets: [
+      { symbol: "BTC", logo: "/icons/btc.svg", contract: "/developers/contracts/atomic-swap-btc" }
+    ]
+  },
+  {
+    chain: { name: "Citrea Testnet", logo: "/icons/citrea.svg" },
+    assets: [
+      { symbol: "WCBTC", logo: "/icons/wbtc.svg", contract: "https://explorer.testnet.citrea.xyz/address/0xaD9d14CA82d9BF97fFf745fFC7d48172A1c0969E?tab=contract" },
+      { symbol: "cBTC", logo: "/icons/btc.svg", contract: "https://explorer.testnet.citrea.xyz/address/0x6218FA00f47a61aCD8B46D6f1b15c4D730F803F2?tab=contract" }
+    ]
+  },
+  {
+    chain: { name: "Monad Testnet", logo: "/icons/monad.svg" },
+    assets: [
+      { symbol: "cbBTC", logo: "/icons/cbbtc.svg", contract: "https://testnet.monadexplorer.com/address/0x1dC94FdcAd8Aee13cfd34Db8a26d26E31572805c?tab=Contract" },
+      { symbol: "USDC", logo: "/icons/usdc.svg", contract: "https://testnet.monadexplorer.com/address/0xE99D8A21d4F2ad2f90c279c869311503f9e25867?tab=Contract" }
+    ]
+  },
+  {
+    chain: { name: "Sepolia", logo: "/icons/ethereum.svg" },
+    assets: [
+      { symbol: "WBTC", logo: "/icons/wbtc.svg", contract: "https://sepolia.etherscan.io/address/0x3C6a17b8cD92976D1D91E491c93c98cd81998265" }
+    ]
+  },
+  {
+    chain: { name: "Solana Testnet", logo: "/icons/solana.png" },
+    assets: [
+      { symbol: "SOL", logo: "/icons/solana.png", contract: "https://explorer.solana.com/address/2bag6xpshpvPe7SJ9nSDLHpxqhEAoHPGpEkjNSv7gxoF?cluster=devnet" }
+    ]
+  },
+  {
+    chain: { name: "Starknet Sepolia", logo: "/icons/starknet.svg" },
+    assets: [
+      { symbol: "WBTC", logo: "/icons/wbtc.svg", contract: "https://sepolia.starkscan.co/contract/0x06579d255314109429a4477d89629bc2b94f529ae01979c2f8014f9246482603" }
+    ]
+  }
+];
+
 
 
 export const SupportedAssets = ({supportedAssetsConfig}) =>{
@@ -88,7 +145,7 @@ export const SupportedAssets = ({supportedAssetsConfig}) =>{
       <tbody>
         {
           supportedAssetsConfig.map((supportedAssetConfig, i)=> (
-            <SupportedAssetRow key={i} assets={supportedAssetConfig.assets} chain={supportedAssetConfig.chain}/>
+            <AssetRow key={i} assets={supportedAssetConfig.assets} chain={supportedAssetConfig.chain}/>
           ))
         }
       </tbody>
@@ -96,7 +153,7 @@ export const SupportedAssets = ({supportedAssetsConfig}) =>{
   )
 }
 
-export const SupportedAssetRow = ({ chain, assets }) => {
+export const AssetRow = ({ chain, assets }) => {
   return (
     <tr>
       <td className="flex items-center gap-2">
@@ -104,7 +161,7 @@ export const SupportedAssetRow = ({ chain, assets }) => {
         <strong>{chain.name}</strong>
       </td>
       <td colSpan={4}>
-        <div className="flex gap-2">
+        <div className="flex gap-4">
           {assets.map((asset, i) => (
             <a key={i} href={asset.contract} className="border-none flex items-center gap-1">
               <img src={asset.logo} width={20} alt={asset.symbol} />
